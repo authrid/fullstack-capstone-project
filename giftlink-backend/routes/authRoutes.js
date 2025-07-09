2f23e099a986b214b6ca4421097ba8729f034caf
Ahmad Ridha <power.of.learn@gmail.com>
2025-07-09 10:26:13 -0400

    Integration for User Profile

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -3,18 +3,16 @@ const express = require('express');
 const app = express();
 const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const { body, validationResult } = require('express-validator');
 const connectToDatabase = require('../models/db');
 const router = express.Router();
 const dotenv = require('dotenv');
 const pino = require('pino');  // Import Pino logger
 
-//Step 1 - Task 3: Create a Pino logger instance
+const { body, validationResult } = require('express-validator');
+
 const logger = pino();  // Create a Pino logger instance
 
 dotenv.config();
-
-//Step 1 - Task 4: Create JWT secret
 const JWT_SECRET = process.env.JWT_SECRET;
 
 router.post('/register', async (req, res) => {
@@ -31,6 +29,7 @@ router.post('/register', async (req, res) => {
         const salt = await bcryptjs.genSalt(10);
         const hash = await bcryptjs.hash(req.body.password, salt);
         const email = req.body.email;
+        console.log('email is',email);
 
         const newUser = await collection.insertOne({
             email: req.body.email,
@@ -56,10 +55,13 @@ router.post('/register', async (req, res) => {
 });
 
 router.post('/login', async (req, res) => {
+    console.log("\n\n Inside login")
+
     try {
         const db = await connectToDatabase();
         const collection = db.collection("users");
         const theUser = await collection.findOne({ email: req.body.email });
+
         if (theUser) {
             let result = await bcryptjs.compare(req.body.password, theUser.password)
             if(!result) {
@@ -78,7 +80,6 @@ router.post('/login', async (req, res) => {
 
             const authtoken = jwt.sign(payload, JWT_SECRET);
             logger.info('User logged in successfully');
-
             return res.status(200).json({ authtoken, userName, userEmail });
 
         } else {
@@ -86,7 +87,62 @@ router.post('/login', async (req, res) => {
             return res.status(404).json({ error: 'User not found' });
         }
     } catch (e) {
+        logger.error(e);
          return res.status(500).send('Internal server error');
+
+    }
+});
+
+router.put('/update', async (req, res) => {
+    
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        logger.error('Validation errors in update request', errors.array());
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        // Check if `email` is present in the header and throw an appropriate error message if not present.
+        const email = req.headers.email;
+        if (!email) {
+            logger.error('Email not found in the request headers');
+            return res.status(400).json({ error: "Email not found in the request headers" });
+        }
+
+        // Connect to MongoDB
+        const db = await connectToDatabase();
+        const collection = db.collection("users");
+
+        // find user credentials in database
+        const existingUser = await collection.findOne({ email });
+
+        if (!existingUser) {
+            logger.error('User not found');
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        existingUser.updatedAt = new Date();
+
+        // Task 6: update user credentials in database
+        const updatedUser = await collection.findOneAndUpdate(
+            { email },
+            { $set: existingUser },
+            { returnDocument: 'after' }
+        );
+        // Task 7: create JWT authentication using secret key from .env file
+        const payload = {
+            user: {
+                id: updatedUser._id.toString(),
+            },
+        };
+
+        const authtoken = jwt.sign(payload, JWT_SECRET);
+        logger.info('User updated successfully');
+
+        res.json({authtoken});
+    } catch (e) {
+        logger.error(e);
+        return res.status(500).send('Internal server error');
 
     }
 });
