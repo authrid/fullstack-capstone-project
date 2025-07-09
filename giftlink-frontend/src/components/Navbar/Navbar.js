083b56063c532b0e0cdbc5aee359c815416f4804
Ahmad Ridha <power.of.learn@gmail.com>
2025-07-09 14:08:39 -0400

    Adding Import useNavigate

--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useEffect} from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import {urlConfig} from '../../config';
 import { useAppContext } from '../../context/AuthContext';
 
@@ -34,7 +34,7 @@ export default function Navbar() {
 
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light" id='navbar_container'>
-            <a className="navbar-brand" href={`${urlConfig.backendUrl}/app`}>GiftLink</a>
+            <Link className="navbar-brand" href={`${urlConfig.backendUrl}/app`}>GiftLink</Link>
 
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
@@ -43,7 +43,7 @@ export default function Navbar() {
             <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item">
-                        <a className="nav-link" href="/home.html">Home</a> 
+                        <Link className="nav-link" href="/home.html">Home</Link> 
                     </li>
                     <li className="nav-item">
                         <Link className="nav-link" to="/app">Gifts</Link>
